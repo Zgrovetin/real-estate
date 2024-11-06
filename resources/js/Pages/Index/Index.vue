5b0defe6f62e17a96dcef7723a889f550a17dbd6
Zgrovetin <534477@gmail.com>
2024-11-06 23:09:55 +0100

    by defining props

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -9,8 +9,14 @@
 <template>
     <div>Index</div>
     <Link href="/hello">Show page</Link>
+    <div>
+        The message is {{message}}
+    </div>
 </template>
 
 <script setup>
     import { Link } from '@inertiajs/vue3'
+    defineProps({
+        message: String
+    })
 </script>
