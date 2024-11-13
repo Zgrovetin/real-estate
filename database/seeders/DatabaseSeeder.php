2cba68ea3b187157f6614f4d55c5b18321fe0774
Zgrovetin <534477@gmail.com>
2024-11-13 10:17:30 +0100

    run composer dumpautoload
    every time when adding new db models and before start working with Tinker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Listing;
+//use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -19,6 +20,6 @@ class DatabaseSeeder extends Seeder
 //            'name' => 'Test User',
 //            'email' => 'test@example.com',
 //        ]);
-        \App\Models\Listing::factory(20)->create();
+        Listing::factory(20)->create();
     }
 }
