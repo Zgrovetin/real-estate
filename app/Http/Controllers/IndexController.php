b4767b6f78784cbde5deeeee34b9412d880a8816
Zgrovetin <534477@gmail.com>
2024-11-13 13:11:51 +0100

    Update IndexController.php

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+// Alternative to using Tinker
+//use App\Models\Listing;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index() {
+        
+// Alternative to using Tinker
+//        dd(Listing::where('beds', '=', 4)->orderBy('price', 'asc')->first());
+
         return inertia(
             'Index/Index',
             [
