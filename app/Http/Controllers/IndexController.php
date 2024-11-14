cd321bf479fc5a2cdb5b05785afbc72da0f7e150
Zgrovetin <534477@gmail.com>
2024-11-14 01:40:22 +0100

    Update IndexController.php

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class IndexController extends Controller
 {
     public function index() {
-        
+
 // Alternative to using Tinker
 //        dd(Listing::where('beds', '=', 4)->orderBy('price', 'asc')->first());
 
