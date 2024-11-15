da1fe05c096f7893f63ec928cd65580015237920
Zgrovetin <534477@gmail.com>
2024-11-15 19:38:06 +0100

    Update MainLayout.vue

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -3,7 +3,10 @@
     <Link href="/">Main page</Link>&nbsp;
     <Link href="/hello">Show page</Link>
 <!--    <div>The page with time {{timer}}</div>-->
-    
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>
+
     <slot>Default</slot>
 </template>
 
@@ -12,9 +15,21 @@
 <!--</style>-->
 
 <script setup>
-    import { Link } from '@inertiajs/vue3'
+    import { computed } from 'vue'
+    import { Link, usePage } from '@inertiajs/vue3'
+
+    // page.props.value.flash.success
+    const page = usePage()
+    const flashSuccess = computed(() => page.props.flash.success)
     // import {ref} from 'vue'
 
     // const timer = ref(0)
     // setInterval(() => timer.value++, 1000)
 </script>
+
+<style scoped>
+    .success {
+        background-color: green;
+        color: white;
+    }
+</style>
