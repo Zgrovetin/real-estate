0c693671b6b1ecf264483160d10f3a653ae7e0bf
Zgrovetin <534477@gmail.com>
2024-11-17 21:45:20 +0100

    for both light and dark mode

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,7 @@
         @routes
         @vite('resources/js/app.js')
     </head>
-    <body class="bg-white dark:bg-gray-900">
+    <body class="bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-300">
         @inertia
     </body>
 </html>
