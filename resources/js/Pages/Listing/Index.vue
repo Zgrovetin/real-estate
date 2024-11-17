f5af58f638656bf7223970fd6746344384960163
Zgrovetin <534477@gmail.com>
2024-11-17 22:23:18 +0100

    UI "library" created

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,25 +1,28 @@
 <template>
-    <div v-for="listing in listings" :key="listing.id">
-        <div>
-            <Link :href="route('listing.show', {id: listing.id})">
-                <ListingAddress :listing="listing" />
-            </Link>
-        </div>
-        <div>
-            <Link :href="route('listing.edit', {id: listing.id})">
-                Edit listing
-            </Link>
-        </div>
-        <div>
-            <Link :href="route('listing.destroy',{id: listing.id})" method="DELETE" as="button">Delete listing</Link>
-        </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <Box v-for="listing in listings" :key="listing.id">
+            <div>
+                <Link :href="route('listing.show', {id: listing.id})">
+                    <ListingAddress :listing="listing" />
+                </Link>
+            </div>
+            <div>
+                <Link :href="route('listing.edit', {id: listing.id})">
+                    Edit listing
+                </Link>
+            </div>
+            <div>
+                <Link :href="route('listing.destroy',{id: listing.id})" method="DELETE" as="button">Delete listing</Link>
+            </div>
+        </Box>
     </div>
 </template>
 
 <script setup>
 
     import {Link} from '@inertiajs/vue3'
-    import ListingAddress from '@/Components/ListingAddress.vue';
+    import ListingAddress from '@/Components/ListingAddress.vue'
+    import Box from '@/Components/UI/Box.vue'
 
     defineProps({
         listings: Array,
