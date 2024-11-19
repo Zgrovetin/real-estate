472deffaf72b36233d6328b7b7280b537640d77f
Zgrovetin <534477@gmail.com>
2024-11-19 14:52:16 +0100

    price can be passed in a slot, without formatting

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -3,6 +3,7 @@
         <Box v-for="listing in listings" :key="listing.id">
             <div>
                 <Link :href="route('listing.show', {id: listing.id})">
+                    <Price :price="listing.price" class="text-2xl font-bold" />
                     <ListingSpace :listing="listing" class="text-lg"/>
                     <ListingAddress :listing="listing" class="text-gray-500"/>
                 </Link>
@@ -25,6 +26,7 @@
     import ListingAddress from '@/Components/ListingAddress.vue'
     import Box from '@/Components/UI/Box.vue'
     import ListingSpace from "@/Components/ListingSpace.vue";
+    import Price from "@/Components/Price.vue";
 
     defineProps({
         listings: Array,
