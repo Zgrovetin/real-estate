0a09693bfc3cb4d32b94dbebd2160f00ac37f239
Zgrovetin <534477@gmail.com>
2024-11-23 19:02:33 +0100

    email or username for the first field

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller
         // optional but VERY IMPORTANT, to prevent potential session fixation attacks !!
         $request->session()->regenerate();
 
-        return redirect()->intended();
+        return redirect()->intended('/listing');
     }
     public function destroy() {
 
