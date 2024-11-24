f863b25e7af12e00ea33d056e3b2451f0acb77da
Zgrovetin <534477@gmail.com>
2024-11-24 21:51:14 +0100

    password_confirmation exact name, tbe

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -18,7 +18,8 @@
                     </div>
 
                 </div>
-                <div v-else>
+                <div v-else class="flex items-center gap-4">
+                    <Link :href="route('user-account.create')">Register</Link>
                     <Link :href="route('login')">Sign-in</Link>
                 </div>
             </nav>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserAccountController;
 
 //Route::get('/', function () {
 ////    return view('welcome');
@@ -21,3 +22,5 @@ Route::resource('listing', ListingController::class)
 Route::get('login', [AuthController::class, 'create'])->name('login');
 Route::post('login', [AuthController::class, 'store'])->name('login.store');
 Route::delete('logout', [AuthController::class, 'destroy'])->name('logout');
+
+Route::resource('user-account', UserAccountController::class)->only(['create']);
