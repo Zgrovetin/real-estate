9157dd1dd4c76499b124d394fb9a9d9e87b65ac2
Zgrovetin <534477@gmail.com>
2024-12-03 18:50:16 +0100

    return is neither necessary in scopeFilter
    nor in scopeMostRecent

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -16,11 +16,37 @@ class Listing extends Model
     ];
 
     public function owner(): BelongsTo {
-        return $this->belongsTo(\App\Models\User::class, 'by_user_id');
+        return $this->belongsTo(User::class, 'by_user_id');
     }
 
     public function scopeMostRecent(Builder $query): Builder
     {
         return $query->orderByDesc('created_at');
     }
+
+    public function scopeFilter (Builder $query, array $filters): Builder
+    {
+        $query->when(
+            $filters['priceFrom'] ?? false,
+            fn ($query, $value) => $query->where('price', '>=', $value)
+        )->when(
+            $filters['priceTo'] ?? false,
+            fn ($query, $value) => $query->where('price', '<=', $value)
+        )->when(
+            $filters['beds'] ?? false,
+            fn ($query, $value) => $query->where('beds', (int)$value < 6 ? '=' : '>=', $value)
+        )->when(
+            $filters['baths'] ?? false,
+            fn ($query, $value) => $query->where('baths', (int)$value < 6 ? '=' : '>=', $value)
+        )->when(
+            $filters['areaFrom'] ?? false,
+            fn ($query, $value) => $query->where('area', '>=', $value)
+        )->when(
+            $filters['areaTo'] ?? false,
+            fn ($query, $value) => $query->where('area', '>=', $value)
+        );
+
+        return $query;
+    }
+
 }
