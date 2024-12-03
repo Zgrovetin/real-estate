67f2f7046356f0ddf247e75d240f67422e150b1a
Zgrovetin <534477@gmail.com>
2024-12-03 18:28:14 +0100

    the import use Illuminate\Database\Eloquent\Builder;
    auto-added

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -16,5 +17,10 @@ class Listing extends Model
 
     public function owner(): BelongsTo {
         return $this->belongsTo(\App\Models\User::class, 'by_user_id');
+    }
+
+    public function scopeMostRecent(Builder $query): Builder
+    {
+        return $query->orderByDesc('created_at');
     }
 }
