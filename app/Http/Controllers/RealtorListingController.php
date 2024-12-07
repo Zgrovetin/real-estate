280736b8f177b4129bc18311bf840ad6d8cc9249
Zgrovetin <534477@gmail.com>
2024-12-07 14:15:39 +0100

    by: price, created_at

--- app/Http/Controllers/RealtorListingController.php
+++ app/Http/Controllers/RealtorListingController.php
@@ -24,13 +24,14 @@ class RealtorListingController extends Controller
         //        dd(Auth::user()->listings);
 
         $filters = [
-            'deleted' => $request->boolean('deleted')
+            'deleted' => $request->boolean('deleted'),
+            ...$request->only(['by', 'order'])
         ];
 
         return inertia('Realtor/Index',
             ['listings' => Auth::user()
                 ->listings()
-                ->mostRecent()
+//                ->mostRecent()
                 ->filter($filters)
                 ->get()
             ]
