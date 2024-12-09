15d11cea47444c621655aaeaed2e7c88fcb7fab9
Zgrovetin <534477@gmail.com>
2024-12-09 08:35:50 +0100

    one to many r

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Listing extends Model
@@ -20,8 +21,14 @@ class Listing extends Model
         'price', 'created_at'
     ];
 
-    public function owner(): BelongsTo {
+    public function owner(): BelongsTo
+    {
         return $this->belongsTo(User::class, 'by_user_id');
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(ListingImage::class);
     }
 
     public function scopeMostRecent(Builder $query): Builder
