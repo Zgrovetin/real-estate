ab6218688a22979d4ef00212bdcdfbeb43c6d285
Zgrovetin <534477@gmail.com>
2024-12-09 12:24:35 +0100

    name="images[]"

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -1,6 +1,8 @@
 <template>
-    <form :action="route('realtor.listing.image.store', { listing: listing.id })"  method="POST">
-        <input type="file" multiple name="files" />
+    <form method="POST"
+          enctype="multipart/form-data"
+          :action="route('realtor.listing.image.store', { listing: listing.id })">
+        <input type="file" multiple name="images[]" />
         <button type="submit">Add Images</button>
     </form>
 </template>
