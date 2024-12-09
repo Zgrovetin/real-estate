88e7f0d70770a2e8c67c770a03f5fca6e30b2582
Zgrovetin <534477@gmail.com>
2024-12-09 11:59:18 +0100

    '/realtor/listing/*' temp excluded from CSRF protection
    to check if the route for adding images works

---
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -0,0 +1,10 @@
+<template>
+    <form :action="route('realtor.listing.image.store', { listing: listing.id })"  method="POST">
+        <input type="file" multiple name="files" />
+        <button type="submit">Add Images</button>
+    </form>
+</template>
+
+<script setup>
+    defineProps({listing: Object})
+</script>
