9789d46437cb06ba9762f5058fb415c8094a1a0d
Zgrovetin <534477@gmail.com>
2024-12-12 15:21:40 +0100

    Querying relationship existence

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -38,6 +38,7 @@ class ListingController extends Controller
                 'filters' => $filters,
                 'listings' => Listing::mostRecent()
                     ->filter($filters)
+                    ->withoutSold()
                     ->Paginate(10)
                     ->withQueryString()
             ]
